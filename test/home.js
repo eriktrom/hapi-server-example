f322f3c24a2aed58266bf3e0cbac3a796c5d83f6
Erik Trom <erik.trom@gmail.com>
2015-08-15 06:07:39 -0700

    add home and login pages for real

--- test/home.js
+++ test/home.js
@@ -16,7 +16,7 @@ var it = lab.test;
 
 describe('/home', function () {
 
-  it('returns the home page containing relative path from root to home template', function (done) {
+  it('returns the home page', function (done) {
 
     Follower.init(internals.manifest, internals.composeOptions, function (err, server) {
 
@@ -27,18 +27,6 @@ describe('/home', function () {
       server.select('web-tls').inject(request, function (res) {
 
         expect(res.statusCode, 'Status code').to.equal(200);
-
-        // output should be: views/home.html
-        var expectedHTMLOutput =
-          Path.relative(
-            Path.resolve('__dirname', '../'),
-            Path.resolve('__dirname', '../views/home.html')
-          );
-
-        // trim actual output to remove trailing newline (\n)
-        var actualHTMLOutput = res.result.trim();
-
-        expect(actualHTMLOutput, 'html output').to.equal(expectedHTMLOutput);
 
         server.stop(done);
       });
